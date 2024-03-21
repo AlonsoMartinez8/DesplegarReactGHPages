9cf3a9e3730d0d1c13efec76fe1ffb7183238b9b
AlonsoMartinez8 <alumno.574777@ies-azarquiel.es>
2024-03-21 19:05:44 +0100

    Update app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
+import gh from './ghlogo.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          APP REACT DESPLEGADA
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href='https://github.com/AlonsoMartinez8'><img src={gh} width="50" alt="github logo" /></a>
       </header>
     </div>
   );
